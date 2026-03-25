20ab49dd880a18d86e54e168a18db148770fa31f
st <st@A510PC3>
2026-03-25 18:38:28 +0300

    add function sum()

--- Task01/test03.cpp
+++ Task01/test03.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-using namespace std;
-
-//base case
-//fib(1) --> 0
-//fib(2) --> 1
-
-//recursion case
-//fib(n-1) + fib(n-2)
-
-long long fibonachi(int index) {
-	if (index <= 0) {
-		return -1;
-	}
-
-	//base-case
-	if (index <= 2) {
-		return index - 1;
-	}
-
-	//recursion-case
-	return fibonachi(index - 1) + fibonachi(index - 2);
-}
-
-
-
-int main() {
-	cout << fibonachi(11) << endl;
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+////base case
+////fib(1) --> 0
+////fib(2) --> 1
+//
+////recursion case
+////fib(n-1) + fib(n-2)
+//
+//long long fibonachi(int index) {
+//	if (index <= 0) {
+//		return -1;
+//	}
+//
+//	//base-case
+//	if (index <= 2) {
+//		return index - 1;
+//	}
+//
+//	//recursion-case
+//	return fibonachi(index - 1) + fibonachi(index - 2);
+//}
+//
+//
+//
+//int main() {
+//	cout << fibonachi(11) << endl;
+//	return 0;
+//}
